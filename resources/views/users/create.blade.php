86fa720afbebcf95ed7ef5eaa82a617b21759599
dipeshbanjara <fadindrarajbanjara.44@gmail.com>
2023-08-26 14:53:13 +0545

    work on user create form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
                     <h2>Create New User</h2>
@@ -11,7 +11,22 @@
                     <a class="btn btn-primary" href="{{ route('users.index') }}"> Back </a>
                 </div>
             </div>
-        </div>
+        </div> --}}
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Create New User</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('users.index') }}">User</a></li>
+                            <li class="breadcrumb-item active">Create User</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
 
 
         @if (count($errors) > 0)
@@ -27,11 +42,12 @@
 
 
 
-        {!! Form::open(['route' => 'users.store', 'method' => 'POST']) !!}
+        {{-- {!! Form::open(['route' => 'users.store', 'method' => 'POST']) !!}
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Name:</strong>
+                    
                     {!! Form::text('name', null, ['placeholder' => 'Name', 'class' => 'form-control']) !!}
                 </div>
             </div>
@@ -63,6 +79,61 @@
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
-        {!! Form::close() !!}
+        {!! Form::close() !!} --}}
+
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- left column -->
+                    <div class="col-md-12">
+                        <!-- jquery validation -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">User Form</small></h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form id="quickForm">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Name:</label>
+                                        <input type="text" name="name" class="form-control" id="name"
+                                            placeholder="Enter Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address:</label>
+                                        <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password:</label>
+                                        <input type="password" name="password" class="form-control"
+                                            id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Confirm Password:</label>
+                                        <input type="password" name="confirm-password" class="form-control"
+                                            id="exampleInputPassword2" placeholder="Re-Enter Password">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!--/.col (left) -->
+                    <!-- right column -->
+                    <div class="col-md-6">
+
+                    </div>
+                    <!--/.col (right) -->
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
     </div>
 @endsection
